9ffee25cb82ac1df0cd65b0ead6757cd5603f698
Armandino <darzins.armands@jak.lv>
2026-03-24 12:21:49 +0000

    Test

---
+++ izstrade/uzdevums1_4.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Uzdevums 1.4. Uzrakstīt PHP kodu, izmantojot komandu echo vai print, kas izvada divu dažādu veselu un pozitīvu skaitļu summu.
+Piemērs:
+Ievades dati:
+13 + 37
+Rezultāts:
+Skaitļa 1.skaitlis un skaitļa 2.skaitlis summa ir: 50
+*/
+
+$skaitlis1 = 13;
+$skaitlis2 = 37;
+
+$summa = $skaitlis1 + $skaitlis2;
+echo "Skaitļa 1.skaitlis un skaitļa 2.skaitlis summa ir: + summa";
